0b91b73481c668a2ad37f891e84ae9d7c9d166cf
Rosie Seok <lovelyrosie731@gmail.com>
2024-10-26 00:02:06 +0900

    user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,36 +1,59 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const saltRounds = 10;
 
 const userController = {};
 
 userController.createUser = async (req, res) => {
   try {
     let { email, password, name, level } = req.body;
-
-    // 이메일로 사용자 중복 확인
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res
-        .status(400)
-        .json({ status: "fail", error: "User already exists." });
+    const user = await User.findOne({ email });
+    
+    if (user) {
+      throw new Error("User already exist.");
     }
-
-    // 비밀번호 해싱
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    // 새로운 사용자 생성
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(password, salt);
     const newUser = new User({
       email,
-      password: hashedPassword,
+      password,
       name,
       level: level ? level : "customer",
     });
     await newUser.save();
-
-    return res.status(201).json({ status: "success" });
+    return res.status(200).json({ status: "success" });
   } catch (error) {
-    res.status(500).json({ status: "fail", error: error.message });
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+userController.loginWithEmail = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }, "-createdAt -updatedAt -__v");
+    if (user) {
+      const isMatch = bcrypt.compareSync(password, user.password);
+      if (isMatch) {
+        const token = user.generateToken();
+        return res.status(200).json({ status: "success", user, token });
+      }
+    }
+    throw new Error("아이디 또는 비밀번호가 일치하지 않습니다.");
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: error.message });
+  }
+};
+
+userController.getUser = async (req, res) => {
+  try {
+    const { userId } = req;
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new Error("can not find user");
+    }
+    res.status(200).json({ status: "success", user });
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: error.message });
   }
 };
 
