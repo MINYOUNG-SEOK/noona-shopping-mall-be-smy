81b1ae277a7ba07714f0fe45b7013a44be16f39b
Rosie Seok <lovelyrosie731@gmail.com>
2024-10-26 21:45:32 +0900

    login

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -7,15 +7,17 @@ const userController = {};
 userController.createUser = async (req, res) => {
   try {
     let { email, password, name, level } = req.body;
+
+    // 이메일 중복 확인
     const user = await User.findOne({ email });
     if (user) {
-      throw new Error("User already exist.");
+      throw new Error("이미 가입된 유저입니다.");
     }
     const salt = bcrypt.genSaltSync(saltRounds);
     const hash = bcrypt.hashSync(password, salt);
     const newUser = new User({
       email,
-      password,
+      password: hash,
       name,
       level: level ? level : "customer",
     });
@@ -24,6 +26,15 @@ userController.createUser = async (req, res) => {
   } catch (error) {
     res.status(400).json({ status: "fail", error: error.message });
   }
+};
+
+userController.checkEmail = async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    return res.status(409).json({ message: "이미 가입된 유저입니다." }); // Conflict
+  }
+  return res.status(200).json({ message: "이메일 사용 가능." }); // OK
 };
 
 userController.loginWithEmail = async (req, res) => {
